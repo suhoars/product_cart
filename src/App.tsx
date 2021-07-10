2fe1605c56721271f9d1fad2074afcd0be4ace2b
st-suho <su.ho@supremetech.vn>
2021-07-10 15:54:46 +0700

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getCategoriesList())
-  }, []);
+  });
   return (
     <BrowserRouter>
       <Suspense fallback>
